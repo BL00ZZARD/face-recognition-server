fe35e0a8ad60ff06aaf44a50a3c33c5dafa00d2a
Lizandro Martinez <lizandro98@outlook.es>
2023-12-13 19:23:49 -0600

    changed something

--- server.js
+++ server.js
@@ -16,15 +16,12 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-    host: process.env.DATABASE_HOST,
-    port: 5432,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PW,
-    database: process.env.DATABASE_DB
+    ssl: true,
   }
 });
 
+
+// ssl: { rejectUnauthorized: false },
 // Create an Express app
 const app = express();
 
