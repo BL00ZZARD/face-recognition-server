ffc946ed2295a3d2d994bbe3ad284fbebe5e10c8
Lizandro Martinez <lizandro98@outlook.es>
2023-12-18 19:12:30 -0600

    update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import knex from 'knex';
 
 // Import controllers
-import registerHandler from './controllers/register.js';
+import register from './controllers/register.js';
 import signin from './controllers/signin.js';
 import profile from './controllers/profile.js';
 import image from './controllers/image.js';
